d86802ccb2faed9e6f127c83a2cc0b067b8e5786
Aralya Phinith <aphinith84@gmail.com>
2016-08-31 15:45:07 -0700

    require chai and coverter file into test file

--- test/converter.js
+++ test/converter.js
@@ -1,3 +1,6 @@
+var expect = require("chai").expect;
+var converter = require("../app/converter");
+
 describe("Color Code Converter", function() {
   describe("RGB to Hex conversion", function() {
     it("converts the basic colors", function() {
@@ -7,7 +10,7 @@ describe("Color Code Converter", function() {
 
   describe("Hex to RGB conversion", function() {
     it("converts teh basic colors", function() {
-      
+
     })
   })
 });
